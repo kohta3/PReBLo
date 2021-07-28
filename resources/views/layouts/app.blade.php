ae8fa9084322716dc667a0be8a0e93108810d108
“kohta3 <gogonta4@gmail.com>
2021-07-28 15:29:05 +0900

    trim-info 0728A

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,8 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -19,6 +18,7 @@
         <!-- Styles -->
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
         <link href="{{ asset('css/PReBLo.css') }}" rel="stylesheet">
+        
     </head>
     <body class="BackGroundColor">
 
