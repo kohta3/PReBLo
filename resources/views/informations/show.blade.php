2a3c3ae69bdf2d3c50185272061d26edf0c3f3f7
“kohta3 <gogonta4@gmail.com>
2021-07-29 22:39:06 +0900

    add show looks

--- resources/views/informations/show.blade.php
+++ resources/views/informations/show.blade.php
@@ -9,9 +9,9 @@
 </div>
 @endif
 
-<div class='m-3 shadow-lg'>
-  <div class="row" style="height: 60rem">
-    <div class="col-sm-7 text-center show-position">
+<div class='m-2 shadow-lg h-60'>
+  <div class="row m-0 p-0">
+    <div class="col-sm-7 show-position">
       @if ($information->image !== "")
         <img src="{{$information->image}}" class="show-image">
         @else
@@ -19,33 +19,34 @@
       @endif
     </div>
 
-    <div class="col-sm-5 show-infor show-position">
-      <div>
-        <?php
-          if($information->ParkingCar===0)$judgeCar='駐車場有り';
-          else$judgeCar='駐車場無し';
-          if($information->ParkingBicycles===0)$judgeBycycle='駐輪場有り';
-          else$judgeBycycle='駐輪場無し';
-        ?>
-        <p>{{$information->tittle}}</p>
-        <p>{{$information->pref}}</p>
-        <p>{{$information->URL}}</p>
-        <p>{{$information->TEL}}</p>
-        <p>{{$information->about}}</p>
-        <p>{{$information->OfficeHour}}</p>
-        <p>{{$judgeCar."\t".$judgeBycycle}}</p>
-        {{-- good or ungood --}}
-      </div>
-
-      <div>
-        @if($information->is_liked_by_auth_user())
-          <a href="{{ route('infounlike', ['id' => $information->id]) }}" class="btn btn-success btn-sm">いいね<span class="badge">{{ $information->likes->count() }}</span></a>
-        @else
-          <a href="{{ route('infolike', ['id' => $information->id]) }}" class="btn btn-info btn-sm">いいね<span class="badge">{{ $information->likes->count() }}</span></a>
-        @endif
-      </div>
-
+    <div class="col-sm-5 show-position show-infor">
+      <?php
+        if($information->ParkingCar===0)$judgeCar='駐車場有り';
+        else$judgeCar='駐車場無し';
+        if($information->ParkingBicycles===0)$judgeBycycle='駐輪場有り';
+        else$judgeBycycle='駐輪場無し';
+      ?>
+      <p class="h2">{{$information->tittle}}</p>
+      <p>{{$information->pref}}</p>
+      <p>{{$information->URL}}</p>
+      <p>{{$information->TEL}}</p>
+      <p>{{$information->about}}</p>
+      <p>{{$information->OfficeHour}}</p>
+      <p>{{$judgeCar."\t".$judgeBycycle}}</p>
+      {{-- good or ungood --}}
+  
+      @if($information->is_liked_by_auth_user())
+        <a href="{{ route('infounlike', ['id' => $information->id]) }}" class="btn btn-success btn-sm">いいね<span class="badge">{{ $information->likes->count() }}</span></a>
+      @else
+        <a href="{{ route('infolike', ['id' => $information->id]) }}" class="btn btn-info btn-sm">いいね<span class="badge">{{ $information->likes->count() }}</span></a>
+      @endif
     </div>
+
+  </div>
+
+
+  <div class="row">
+
   </div>
 </div>
 
