cfc816a477184f99a985e6d11e112abf750f6a57
“kohta3 <gogonta4@gmail.com>
2021-08-03 21:22:44 +0900

    0803A

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta name="description" content="投稿型旅行サイト!!PReBLo! 観光地、宿泊施設、飲食店をみんなに共有しよう！">
+        <meta name="keywords" content="旅行,宿泊,ホテル,観光,宿,ご飯,レストラン,旅">
+        <meta name="google-site-verification" content="google-site-verification=N5XjBu4b5Ku1mI9ahQI_C497GEJLYl1fInfBoUgZ8uM">
+
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
