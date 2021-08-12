b46cb9a370f7f3fb0d01faec4561588a6a688171
“kohta3 <gogonta4@gmail.com>
2021-08-12 17:50:27 +0900

    tweetMeta

--- resources/views/informations/show.blade.php
+++ resources/views/informations/show.blade.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="name"  content="投稿型旅行サイト!!PReBLo!">    
-  <meta property="og:url" content=" www.preblo.site" />
-  <meta property="og:type" content="website" />
-  <meta property="og:title" content="PReBLo!!{{$information->tittle}}を共有" />
-  <meta property="og:description" content=" 観光地、宿泊施設、飲食店をみんなに共有しよう！" />
-  <meta property="og:site_name" content="PReBLo" />
-  <meta property="og:image" content="{{$information->image}}" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="name"  content="投稿型旅行サイト!!PReBLo!">    
+    <meta property="og:url" content=" www.preblo.site" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="投稿型旅行サイト!!PReBLo" />
+    <meta property="og:description" content=" 観光地、宿泊施設、飲食店をみんなに共有しよう！" />
+    <meta property="og:site_name" content="PReBLo" />
+    <meta property="og:image" content={{asset('img/meta.png')}} />
 </head>
+
+
+
 <body>
   @extends('layouts.app')
   @section('content')
