f0c0ef66b5348ac6556b2fe5dcdeec2c988dcc47
“kohta3 <gogonta4@gmail.com>
2021-08-16 20:09:01 +0900

    0816

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>PReBLo</title>
+        <title>観光地・ホテル・ご飯の投稿サイトPReBLo!</title>
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
