fc4d4ff67b60e23305afab104eb34101d8f34b93
“kohta3 <gogonta4@gmail.com>
2021-09-09 20:45:56 +0900

    0908

--- resources/views/firstcontact.blade.php
+++ resources/views/firstcontact.blade.php
@@ -26,7 +26,7 @@
                         <img class="d-block w-85 img-fluid" src="{{asset('img/carucell01.jpg')}}" alt="d-block img-fluid">
                         {{-- <div class="mr-5 text-center"> --}}
                             <div class="firstContact_button font-weight-bold w-25 text-white">
-                                <a href="{{ route('register') }}"><button class="btn btn-success col btn-lg btn-block mb-4 text-nowrap">新規登録</button></a>
+                                <a href="{{ route('register') }}"><button class="btn btn-success col btn-lg btn-block mb-3 text-nowrap">新規登録</button></a>
                                 <a href="{{ route('login') }}"><button class="btn btn-info col btn-lg btn-block text-nowrap">ログイン</button></a>
                             </div>
                     </div>
@@ -35,7 +35,7 @@
                         <img class="d-block w-85 img-fluid" src="{{asset('img/carucell02.jpg')}}" alt="d-block img-fluid">
                         {{-- <div class="mr-5 text-center"> --}}
                         <div class="firstContact_button font-weight-bold w-25 text-white">
-                            <a href="{{ route('register') }}"><button class="btn btn-success col btn-lg btn-block mb-4 text-nowrap">新規登録</button></a>
+                            <a href="{{ route('register') }}"><button class="btn btn-success col btn-lg btn-block mb-3 text-nowrap">新規登録</button></a>
                             <a href="{{ route('login') }}"><button class="btn btn-info col btn-lg btn-block text-nowrap">ログイン</button></a>
                         </div>
                     </div>
