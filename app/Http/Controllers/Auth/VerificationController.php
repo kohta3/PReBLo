01113cb24e571df86fbf9e2ab4a170a38fb97469
“kohta3 <gogonta4@gmail.com>
2021-09-23 18:19:41 +0900

    0923

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Information;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\VerifiesEmails;
 
@@ -26,7 +27,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/Information'; //RouteServiceProvider::HOME
 
     /**
      * Create a new controller instance.
